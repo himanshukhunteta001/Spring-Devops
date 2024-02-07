c2822aa82f55d8d6cdc266f2df987e69769173b9
himanshukhunteta001 <153600953+himanshukhunteta001@users.noreply.github.com>
2024-02-07 14:51:06 +0530

    Update SpringBootDockerApplication.java

--- src/main/java/com/springBootDocker/SpringBootDockerApplication.java
+++ src/main/java/com/springBootDocker/SpringBootDockerApplication.java
@@ -2,9 +2,17 @@ package com.springBootDocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerApplication {
+
+	@GetMapping("/start")
+	public String getMessage () {
+		return "Started the spring boot api application using the Docker.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerApplication.class, args);
